6b4cf71833a6efac6446d90f03c328fa30bac9b6
Rukaroa <rukaroa@gmail.com>
2017-04-19 19:40:24 -0500

    added file requires

--- server.js
+++ server.js
@@ -12,16 +12,17 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({
-  type: "application/vnd.api+json"
+    type: "application/vnd.api+json"
 }));
 
-//friend database array
-var friends = []
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+require("./app/data/friends")(app);
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
