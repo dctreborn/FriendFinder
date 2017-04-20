d1673fbbd8276aed2e5d2dd1486223edee3fb269
Rukaroa <rukaroa@gmail.com>
2017-04-19 19:38:56 -0500

    base data added

---
+++ app/data/friends.js
@@ -0,0 +1,189 @@
+//friend database array
+var friends = [{
+	    "name": "Ahmed",
+	    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	    "scores": [
+	        "5",
+	        "1",
+	        "4",
+	        "4",
+	        "5",
+	        "1",
+	        "2",
+	        "5",
+	        "4",
+	        "1"
+	    ]
+
+	}, {
+
+	    "name": "Jacob Deming",
+	    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+	    "scores": [
+	        "4",
+	        "2",
+	        "5",
+	        "1",
+	        "3",
+	        "2",
+	        "2",
+	        "1",
+	        "3",
+	        "2"
+	    ]
+
+	}, {
+
+	    "name": "Jeremiah Scanlon",
+	    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+	    "scores": [
+	        "5",
+	        "2",
+	        "2",
+	        "2",
+	        "4",
+	        "1",
+	        "3",
+	        "2",
+	        "5",
+	        "5"
+	    ]
+
+	}, {
+
+	    "name": "Louis T. Delia",
+	    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+	    "scores": [
+	        "3",
+	        "3",
+	        "4",
+	        "2",
+	        "2",
+	        "1",
+	        "3",
+	        "2",
+	        "2",
+	        "3"
+	    ]
+
+	}, {
+
+	    "name": "Lou Ritter",
+	    "photo": "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+	    "scores": [
+	        "4",
+	        "3",
+	        "4",
+	        "1",
+	        "5",
+	        "2",
+	        "5",
+	        "3",
+	        "1",
+	        "4"
+	    ]
+
+	}, {
+
+	    "name": "Jordan Biason",
+	    "photo": "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+	    "scores": [
+	        "4",
+	        "4",
+	        "2",
+	        "3",
+	        "2",
+	        "2",
+	        "3",
+	        "2",
+	        "4",
+	        "5"
+	    ]
+
+	}, {
+
+	    "name": "Sen",
+	    "photo": "https://goo.gl/photos/w16naBtSiNoYhHYb7",
+	    "scores": [
+	        "5",
+	        "3",
+	        "3",
+	        "1",
+	        "2",
+	        "2",
+	        "2",
+	        "2",
+	        "1",
+	        "2"
+	    ]
+
+	}, {
+
+	    "name": "Ronald Mcdonald",
+	    "photo": "http://vignette2.wikia.nocookie.net/vsbattles/images/c/c2/Ash_Ketchum_BW.png/revision/latest?cb=20161111173131",
+	    "scores": [
+	        "4",
+	        "3",
+	        "4",
+	        "3",
+	        "4",
+	        "5",
+	        "4",
+	        "3",
+	        "5",
+	        "4"
+	    ]
+
+	}, {
+
+	    "name": "Zach B",
+	    "photo": "n/a",
+	    "scores": [
+	        "5",
+	        "5",
+	        "4",
+	        "2",
+	        "1",
+	        "2",
+	        "2",
+	        "2",
+	        "5",
+	        "3"
+	    ]
+
+	}, {
+
+	    "name": "Paul",
+	    "photo": "https://wiki.mafiascum.net/index.php?title=File:T-cop.png",
+	    "scores": [
+	        "2",
+	        "3",
+	        "4",
+	        "3",
+	        "2",
+	        "3",
+	        "4",
+	        "3",
+	        "2",
+	        "3"
+	    ]
+
+	}, {
+
+	    "name": "Bob Smith",
+	    "photo": "https://upload.wikimedia.org/wikipedia/commons/f/f2/Robert_C_Smith.jpg",
+	    "scores": [
+	        "2",
+	        "2",
+	        "2",
+	        "2",
+	        "3",
+	        "2",
+	        "3",
+	        "2",
+	        "2",
+	        "3"
+	    ]
+
+	}
+]
