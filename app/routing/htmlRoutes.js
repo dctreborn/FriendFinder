d7019682b3e17c68d3e2a05a82de8e3ebc23c6b7
Rukaroa <rukaroa@gmail.com>
2017-04-21 16:29:50 -0500

    minor changes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = function(app) {
 	//root
-    app.get("/", function(req, res) {
+    app.use("/", function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 
